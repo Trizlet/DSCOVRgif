d89cd9c9a376a1f0f1f117443663af9c21cfb4de
Trizlet <91354780+Trizlet@users.noreply.github.com>
2023-07-27 21:56:52 -0400

    Update DSCOVRgif.py

--- DSCOVRgif.py
+++ DSCOVRgif.py
@@ -7,9 +7,15 @@ import urllib.request
 import imageio.v2 as imageio
 import requests
 
-# select image type
-parser = argparse.ArgumentParser()
-parser.add_argument('-t','--type', type=str, required=False, choices=['n', 'e', 'a','c'], default="n")
+# select arguments
+parser = argparse.ArgumentParser(
+                    prog=__file__,
+                    description='A tool to create gifs from imagery gathered by the EPIC instrument onboard the DSCOVR spacecraft.',
+                    epilog='@Trizlet was here :P')
+parser.add_argument('-t','--type', type=str, required=False, choices=['n', 'e', 'a','c'], default="n", help = 'to select natural, enhanced, aerosol, or cloud imagery. (Natural on default)')
+parser.add_argument('-nr', '--noremove', action='store_true', help = 'to keep downloaded images after use (Deleted on default)')
+parser.add_argument('-nl', '--noloop', action='store_true', help = 'disable gif looping. (Loops on default)')
+parser.add_argument('-v', '--verbose', action='store_true', help = 'to see what images are being downloaded (Disabled on default)')
 args = parser.parse_args()
 if args.type == "n":
     type = "natural"
@@ -22,7 +28,7 @@ elif args.type == "c":
 else: 
     type ="natural"
 
-# get latest image data
+# get latest image data from API
 response = requests.get("https://epic.gsfc.nasa.gov/api/" + type)
 if str(response.status_code) != "200":
     print("API request failed! returned " + str(response.status_code))
@@ -32,12 +38,17 @@ date = list[0]["date"][:10]
 # create folder
 folder = "./tempImages" # where to save images
 if os.path.exists(folder + "/"):
-    print(folder + " already exists! stopping just in case.")
-    exit()
+    if input(folder+ " already exists! Would you like to write over it? this will delete the file! (y/n)\n").strip().lower() == "y":
+        shutil.rmtree(folder)
+        os.makedirs(folder + "/")
+    else:
+        print("stopping.")
+        exit()
 if not os.path.exists(folder + "/"):
     os.makedirs(folder + "/")
 
 # get images
+print("Downloading images...")
 for x, item in enumerate(list):
     url = (
         "https://epic.gsfc.nasa.gov/archive/"+type+"/" 
@@ -51,25 +62,32 @@ for x, item in enumerate(list):
         + ".png"
     )
     output = (
-        "./tempImages/"
+        folder+"/"
         + list[x]["image"]
         + ".png"
     )
-    print("saving " + output)
+    if args.verbose:
+        print("saving " + output)
     urllib.request.urlretrieve(url, output)
 
-gif = type + "_" + date + ".gif"
-print("creating animation at " + os.getcwd() + " please wait..")
+gif = type + "_" + date + ".gif" # remove the _ and date from this if you just want them to come out as the same filename every day
+print("Creating animation at " + os.getcwd() + " please wait...")
 
+# create gif
 images = []
 for file_name in sorted(os.listdir(folder)):
     if file_name.endswith(".png"):
         file_path = os.path.join(folder, file_name)
         images.append(imageio.imread(file_path))
-imageio.mimsave(
-    "./" + gif, images,
-)
 
-shutil.rmtree("./tempImages") # remove this if you want to keep the saved images
+if not args.noloop:
+    imageio.mimsave(
+    "./" + gif, images, loop = 0
+    )
+else:
+    imageio.mimsave(
+        "./" + gif, images
+    )
+if not args.noremove:
+    shutil.rmtree(folder) # removes image folder
 print(gif + " successfully created at " + os.getcwd())
-
