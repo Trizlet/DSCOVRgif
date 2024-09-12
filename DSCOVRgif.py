4a7e3314f22ebf914f6d3ae5669b515e32f35131
Trizlet <91354780+Trizlet@users.noreply.github.com>
2024-09-12 14:28:19 -0400

    Cleaning

--- DSCOVRgif.py
+++ DSCOVRgif.py
@@ -1,4 +1,3 @@
-# this works but totally isnt perfect, so feel free to do what you want with it!
 import argparse
 import os
 import shutil
@@ -9,13 +8,37 @@ import requests
 
 # select arguments
 parser = argparse.ArgumentParser(
-                    prog=__file__,
-                    description='A tool to create gifs from imagery gathered by the EPIC instrument onboard the DSCOVR spacecraft.',
-                    epilog='@Trizlet was here :P')
-parser.add_argument('-t','--type', type=str, required=False, choices=['n', 'e', 'a','c'], default="n", help = 'to select natural, enhanced, aerosol, or cloud imagery. (Natural on default)')
-parser.add_argument('-nr', '--noremove', action='store_true', help = 'to keep downloaded images after use (Deleted on default)')
-parser.add_argument('-nl', '--noloop', action='store_true', help = 'disable gif looping. (Loops on default)')
-parser.add_argument('-v', '--verbose', action='store_true', help = 'to see what images are being downloaded (Disabled on default)')
+    prog=__file__,
+    description="A tool to create gifs from the latest imagery gathered by the EPIC instrument onboard the DSCOVR spacecraft.",
+    epilog="@Trizlet was here :P",
+)
+parser.add_argument(
+    "-t",
+    "--type",
+    type=str,
+    required=False,
+    choices=["n", "e", "a", "c"],
+    default="n",
+    help="to select natural, enhanced, aerosol, or cloud imagery. (Natural on default)",
+)
+parser.add_argument(
+    "-nr",
+    "--noremove",
+    action="store_true",
+    help="to keep downloaded images after use (Deleted on default)",
+)
+parser.add_argument(
+    "-nl",
+    "--noloop",
+    action="store_true",
+    help="disable gif looping. (Loops on default)",
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="to see what images are being downloaded (Disabled on default)",
+)
 args = parser.parse_args()
 if args.type == "n":
     type = "natural"
@@ -25,8 +48,8 @@ elif args.type == "a":
     type = "aerosol"
 elif args.type == "c":
     type = "cloud"
-else: 
-    type ="natural"
+else:
+    type = "natural"
 
 # get latest image data from API
 response = requests.get("https://epic.gsfc.nasa.gov/api/" + type)
@@ -36,9 +59,19 @@ list = response.json()
 date = list[0]["date"][:10]
 
 # create folder
-folder = os.path.dirname(os.path.realpath(__file__)) + "/tempImages" # where to save images
+folder = (
+    os.path.dirname(os.path.realpath(__file__)) + "/tempImages"
+)  # where to save images
 if os.path.exists(folder + "/"):
-    if input(folder+ " already exists! Would you like to write over it? this will delete the file! (y/n)\n").strip().lower() == "y":
+    if (
+        input(
+            folder
+            + " already exists! Would you like to write over it? this will delete the file! (y/n)\n"
+        )
+        .strip()
+        .lower()
+        == "y"
+    ):
         shutil.rmtree(folder)
         os.makedirs(folder + "/")
     else:
@@ -51,7 +84,9 @@ if not os.path.exists(folder + "/"):
 print("Downloading images...")
 for x in range(len(list)):
     url = (
-        "https://epic.gsfc.nasa.gov/archive/"+type+"/" 
+        "https://epic.gsfc.nasa.gov/archive/"
+        + type
+        + "/"
         + list[x]["date"][:4]
         + "/"
         + list[x]["date"][5:7]
@@ -61,17 +96,15 @@ for x in range(len(list)):
         + list[x]["image"]
         + ".png"
     )
-    output = (
-        folder+"/"
-        + list[x]["image"]
-        + ".png"
-    )
+    output = folder + "/" + list[x]["image"] + ".png"
     if args.verbose:
         print("saving " + url)
     urllib.request.urlretrieve(url, output)
 
-gif = type + "_" + date + ".gif" # remove the _ and date from this if you just want them to come out as the same filename every day
-print("Creating animation at " + os.getcwd() + " please wait...")
+gif = (
+    type + "_" + date + ".gif"
+)  # remove the _ and date from this if you just want them to come out as the same filename every day
+print("Creating animation at " + os.getcwd() + " ...")
 
 # create gif
 images = []
@@ -82,13 +115,19 @@ for file_name in sorted(os.listdir(folder)):
 
 if not args.noloop:
     imageio.mimsave(
-    os.path.dirname(os.path.realpath(__file__))+ "/" + gif, images, loop = 0
+        os.path.dirname(os.path.realpath(__file__)) + "/" + gif,
+        images,
+        loop=0,
+        format="GIF",
+        duration=500,
     )
 else:
     imageio.mimsave(
-        os.path.dirname(os.path.realpath(__file__))+ "/" + gif, images
+        os.path.dirname(os.path.realpath(__file__)) + "/" + gif,
+        images,
+        format="GIF",
+        duration=500,
     )
 if not args.noremove:
-    shutil.rmtree(folder) # removes image folder
+    shutil.rmtree(folder)  # removes image folder
 print(gif + " successfully created at " + os.path.dirname(os.path.realpath(__file__)))
-
